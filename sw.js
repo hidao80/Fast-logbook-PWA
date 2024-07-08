7935428eb2ca905c654251c4067a8e6fa827af99
hidao80 <8155294+hidao80@users.noreply.github.com>
2024-07-09 06:58:13 +0900

    fix version number

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const APP_NAME = "Fast logbook lite";
-const VERSION = "202407090643JST";
+const VERSION = "202407090655JST";
 
 // Key string indicating that this is cache data for this service worker
 // By including the version number, we aim to update cache files when the source is updated
